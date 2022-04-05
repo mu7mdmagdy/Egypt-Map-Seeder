d9fc101d65f62e7f3d090b7bd0c8478b01cbdf1c
Mohamed Magdy <63126868+mu7mdmagdy@users.noreply.github.com>
2022-04-05 02:34:46 +0200

    Create areas.php

---
+++ database/migrations/areas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->json('title')->comment('{"en":"title","ar":"العنوان"}');
+            $table->foreignId('city_id')
+                ->constrained('cities')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+};
+
